084ed3760014b49ec1593023d4fb1c6ddcff6548
Takuya Kawabuchi <tbuchi888@users.noreply.github.com>
2022-03-22 18:02:18 +0900

    Update time-since-test.go

--- time-since-test.go
+++ time-since-test.go
@@ -7,18 +7,18 @@ import (
 
 func main() {
   
-    t0 := time.Now() 
+    t0 := time.Now()    
     t1 := time.Now() 
-	since :=  time.Since(t0)
-
+    since :=  time.Since(t0)
+    
     fmt.Println("t0:",t0)	
-    fmt.Println("t1:",t1)	
+    fmt.Println("t1:",t1)
     fmt.Println("time.Since(t0):", since)
 
+    fmt.Println("Execute time.Now() and fmt.Println 10 times with for statement")
     for i := 0; i < 10; i++ {
         t := time.Now()   
-		fmt.Println( t )
+	fmt.Println( t )
     }   
-
 
 }
