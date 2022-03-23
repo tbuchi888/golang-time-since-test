a970afc90658470167dde45075e25f8ddb49733a
Takuya Kawabuchi <tbuchi888@users.noreply.github.com>
2022-03-23 03:22:21 +0000

    Corrected line breaks

--- time-since-test.go
+++ time-since-test.go
@@ -1,24 +1,24 @@
 package main
-  
+
 import (
-    "fmt"
-    "time"
+	"fmt"
+	"time"
 )
 
 func main() {
-  
-    t0 := time.Now()    
-    t1 := time.Now() 
-    since :=  time.Since(t0)
-    
-    fmt.Println("t0:",t0)	
-    fmt.Println("t1:",t1)
-    fmt.Println("time.Since(t0):", since)
 
-    fmt.Println("Execute time.Now() and fmt.Println 10 times with for statement")
-    for i := 0; i < 10; i++ {
-        t := time.Now()   
-	fmt.Println( t )
-    }   
+	t0 := time.Now()
+	t1 := time.Now()
+	since := time.Since(t0)
+
+	fmt.Println("t0:", t0)
+	fmt.Println("t1:", t1)
+	fmt.Println("time.Since(t0):", since)
+
+	fmt.Println("Execute time.Now() and fmt.Println 10 times with for statement")
+	for i := 0; i < 10; i++ {
+		t := time.Now()
+		fmt.Println(t)
+	}
 
 }
